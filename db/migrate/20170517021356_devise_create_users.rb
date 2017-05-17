dec165181b5c9900b706ed8930ef3143c2b2605e
dejo-olakareem <oladejoolakareem@yahoo.com>
2017-05-16 19:22:01 -0700

    Add phone number and name to migration for devise

--- db/migrate/20170517021356_devise_create_users.rb
+++ db/migrate/20170517021356_devise_create_users.rb
@@ -5,6 +5,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      #added info
+      t.string :name
+      t.integer :phone_number
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
